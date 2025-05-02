28ed863d40359d2215308b5747de3cd75bc326b2
Fireblade <72758695+Firebladedoge229@users.noreply.github.com>
2025-05-02 14:01:27 -0400

    bypass invalid certificate

--- src/stagescan.rs
+++ src/stagescan.rs
@@ -74,25 +74,38 @@ fn get_jz_that_controls_internal_studio(random_string_internal_studio: u64) -> O
 }
 
 pub fn start(mut input: Vec<u8>, output: &PathBuf) {
-    let pe = PE::parse(&input).unwrap();
-    let str_addr = find_this_random_string_that_lets_us_get_internal_studio(&pe, &input).expect("Error: Could not find the string that is searched for to get internal studio. Please report to https://github.com/7ap/internal-studio-patcher/issues");
+    let pe = PE::parse(&input).unwrap_or_else(|e| {
+        eprintln!("Error parsing PE file: {}", e);
+        std::process::exit(1);
+    });
+
+    if let Err(cert_error) = pe.certificates() {
+        eprintln!("Warning: Failed to extract certificate: {}", cert_error);
+    }
+
+    let str_addr = find_this_random_string_that_lets_us_get_internal_studio(&pe, &input)
+        .expect("Error: Could not find the string that is searched for to get internal studio.");
+    
     let text = pe
         .sections
         .iter()
         .find(|s| s.name().unwrap_or_default() == ".text")
         .expect(
-            ".text missing (this error isnt formatted because this literally should never happen)",
+            ".text missing (this error isn't formatted because this literally should never happen)",
         );
+
     let raw_start = text.pointer_to_raw_data as usize;
     let raw_size = text.size_of_raw_data as usize;
     let text_start = (pe.image_base as u64) + text.virtual_address as u64;
     let text_bytes = &input[raw_start..raw_start + raw_size];
+    
     let mut dec = Decoder::with_ip(64, text_bytes, text_start, DecoderOptions::NONE);
     let mut instruction = Instruction::default();
     while dec.can_decode() {
         dec.decode_out(&mut instruction);
         INSTRUCTIONS.lock().unwrap().push(instruction);
     }
+
     let patch_me = get_jz_that_controls_internal_studio(str_addr);
     if let Some(patch_me) = patch_me {
         let offset = raw_start + (patch_me - text_start) as usize;
@@ -103,9 +116,12 @@ pub fn start(mut input: Vec<u8>, output: &PathBuf) {
             .find(|i| i.ip() == patch_me)
             .map(|i| i.len())
             .unwrap_or(0);
+        
         input[offset] = 0x75;
         input[offset + 1] = 0x05;
+        
         std::fs::write(output, &input).unwrap();
+        println!("File successfully patched!");
     } else {
         eprintln!("Error: Could not find the address to patch. Please report to https://github.com/7ap/internal-studio-patcher/issues");
         std::process::exit(1);
